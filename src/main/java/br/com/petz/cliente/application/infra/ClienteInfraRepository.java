9b1186717c386538dd0508550bdbaaa91d8381f0
Danifleikson Buriti Lisboa <danifleikson@hotmail.com>
2023-01-31 20:45:57 -0300

    Implementa o tratamento de erro de exception duplicada.

--- src/main/java/br/com/petz/cliente/application/infra/ClienteInfraRepository.java
+++ src/main/java/br/com/petz/cliente/application/infra/ClienteInfraRepository.java
@@ -3,6 +3,7 @@ package br.com.petz.cliente.application.infra;
 import java.util.List;
 import java.util.UUID;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Repository;
 
@@ -22,7 +23,14 @@ public class ClienteInfraRepository implements ClienteRepository {
 	@Override
 	public Cliente salva(Cliente cliente) {
 		log.info("[Inicia] ClienteInfraRepository - salvar");
-		clienteSpringDataJpa.save(cliente);
+		try {
+			clienteSpringDataJpa.save(cliente);
+			
+		}catch(DataIntegrityViolationException e){
+			throw APIException.build(HttpStatus.BAD_REQUEST,"Existem dados duplicados", e);
+			
+		}
+		
 		log.info("[Finaliza] ClienteInfraRepository - salvar");
 		
 		return cliente;
